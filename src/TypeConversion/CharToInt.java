e0b9777b222c8510734e5140e5c096432af8aabe
Ridhya Punj <ridhyapunj@gmail.com>
2024-08-30 21:04:38 +0530

    conversion

---
+++ src/TypeConversion/CharToInt.java
@@ -0,0 +1,21 @@
+package TypeConversion;
+
+public class CharToInt {
+    public static void main(String[] args) {
+
+        char a=  '*';
+        int num=a;
+        System.out.println(num);
+    }
+}
+
+
+/**
+public class CharToInt {
+    public static void main(String[] args) {
+        int num=97773344;
+        char a= (char) num;
+        System.out.println(a);
+    }
+}
+*/
