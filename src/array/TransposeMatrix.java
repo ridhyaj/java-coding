33e09eb278edda47f86e943274fc5695c268bb97
Ridhya Punj <ridhyapunj@gmail.com>
2024-09-01 20:56:44 +0530

    Transpose Matrix

---
+++ src/array/TransposeMatrix.java
@@ -0,0 +1,29 @@
+package array;
+
+import java.util.Scanner;
+
+public class TransposeMatrix {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int m=s.nextInt();
+        int n=s.nextInt();
+        int[][] a=new int[m][n];
+        int[][] b=new int[m][n];
+        for (int i=0;i<m;i++) {
+            for (int j = 0; j < n; j++) {
+                a[i][j] = s.nextInt();
+            }
+        }
+        for (int i=0;i<m;i++){
+            for (int j=0;j<n;j++){
+                b[i][j]=a[j][i];
+            }
+        }
+        for (int i=0;i<m;i++){
+            for (int j=0;j<n;j++){
+                System.out.print(b[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
