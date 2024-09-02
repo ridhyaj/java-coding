336f19a34a0a203e94467d6bad8d29b790510d10
Ridhya Punj <ridhyapunj@gmail.com>
2024-09-02 15:46:35 +0530

    max element in an array

---
+++ src/array/FindMaxElement.java
@@ -0,0 +1,24 @@
+package array;
+
+import java.util.Scanner;
+
+public class FindMaxElement
+{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] a=new int[n];
+        for (int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        int max=a[0];
+        for (int i=0;i<n;i++){
+            if(a[i]>max){
+              max=a[i];
+            }
+        }
+        System.out.println("the largest element is "+max);
+
+
+    }
+}
