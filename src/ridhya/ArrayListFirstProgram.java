f264752da36a73ff5e2f43c0db6ce76d1a26adb8
Ridhya Punj <ridhyapunj@gmail.com>
2024-09-14 21:24:18 +0530

    ArrayList

---
+++ src/ridhya/ArrayListFirstProgram.java
@@ -0,0 +1,17 @@
+package ridhya;
+
+import java.util.ArrayList;
+
+public class ArrayListFirstProgram {
+    public static void main(String[] args) {
+        ArrayList <Integer> l=new ArrayList<>();
+        l.add(5);
+        l.add(7);
+        l.add(9);
+        l.add(5);
+        for (int ele : l) {
+            System.out.println(ele);
+        }
+
+    }
+}
